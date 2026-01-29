195877538d90b33d44da91a17b8007196d18261a
Mohamed Faaris <mohamedfaaris.dev@gmail.com>
2026-01-29 16:27:55 +0530

    init

---
+++ src/server/db/schema.ts
@@ -0,0 +1,225 @@
+import { relations } from "drizzle-orm";
+import { convertIndexToString } from "drizzle-orm/mysql-core";
+import {
+  index,
+  pgTableCreator,
+  pgEnum,
+  uniqueIndex,
+  pgTable,
+} from "drizzle-orm/pg-core";
+
+/* -------------------- TYPES -------------------- */
+
+
+/**
+ * Multi-project schema creator
+ */
+
+export const createTable = pgTableCreator(
+  (name) => `Learning_portal_${name}`
+);
+
+
+/* 1. Managers (needs roles enum) */
+export const rolesEnum = pgEnum("roles", ["admin", "manager", "staff"]);
+
+export const college = pgEnum("college", ["krce", "krct", "mkce"])
+
+export const department = pgEnum("department", ["CSE", "EEE", "ECE", "AI", "AIDS"])
+
+export const videoEvents = pgEnum("video_events", ["pause", "stop", "start"])
+
+
+/* -------------------- AUTH TABLES -------------------- */
+
+export const user = pgTable("user", (d) => ({
+  id: d.text().primaryKey(),
+  name: d.text().notNull(),
+  college: college("college").notNull(),
+  department: department("department").notNull(),
+  role: rolesEnum("role").notNull(),
+  email: d.text().notNull().unique(),
+  emailVerified: d.boolean().$defaultFn(() => false).notNull(),
+  image: d.text(),
+  createdAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+  updatedAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}), (t) => ({
+  collegeIdx: index('college_dept_id_idx').on(t.college, t.department, t.id)
+}));
+
+export const session = pgTable("session", (d) => ({
+  id: d.text().primaryKey(),
+  expiresAt: d.timestamp().notNull(),
+  token: d.text().notNull().unique(),
+  createdAt: d.timestamp().notNull(),
+  updatedAt: d.timestamp().notNull(),
+  ipAddress: d.text(),
+  userAgent: d.text(),
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+}));
+
+export const account = pgTable("account", (d) => ({
+  id: d.text().primaryKey(),
+  accountId: d.text().notNull(),
+  providerId: d.text().notNull(),
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+  accessToken: d.text(),
+  refreshToken: d.text(),
+  idToken: d.text(),
+  accessTokenExpiresAt: d.timestamp(),
+  refreshTokenExpiresAt: d.timestamp(),
+  scope: d.text(),
+  password: d.text(),
+  createdAt: d.timestamp().notNull(),
+  updatedAt: d.timestamp().notNull(),
+}));
+
+export const verification = pgTable("verification", (d) => ({
+  id: d.text().primaryKey(),
+  identifier: d.text().notNull(),
+  value: d.text().notNull(),
+  expiresAt: d.timestamp().notNull(),
+  createdAt: d.timestamp().$defaultFn(() => new Date()),
+  updatedAt: d.timestamp().$defaultFn(() => new Date()),
+}));
+
+
+/* -------------------- TABLES -------------------- */
+
+
+
+export const userMeta = pgTable("user_meta", (d) => ({
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+  phone_number: d.text(),
+  address: d.text(),
+  // Add other metadata fields as needed
+
+  createdAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+  updatedAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}));
+
+export const course = pgTable("course", (d) => ({
+  id: d.serial().primaryKey(),
+  title: d.text().notNull(),
+  description: d.text(),
+  createdAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+  updatedAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}));
+
+export const content = pgTable("content", (d) => ({
+  id: d.serial().primaryKey(),
+  courseId: d.integer().notNull().references(() => course.id, { onDelete: "cascade" }),
+  order: d.integer().notNull(),
+  title: d.text().notNull(),
+  body: d.text().notNull(),
+  videoId: d.text().notNull(),
+  createdAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+  updatedAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}));
+
+export const comments = pgTable("comments", (d) => ({
+  id: d.serial().primaryKey(),
+  contentId: d.integer().notNull().references(() => content.id, { onDelete: "cascade" }),
+  parentCommentId: d.integer(),
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+  commentText: d.text().notNull(),
+  createdAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+  updatedAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}), (t) => ({
+  parentReference: index('parent_comment_idx').on(t.parentCommentId)
+}));
+
+
+export const endQuiz = pgTable("end_quiz", (d) => ({
+  id: d.serial().primaryKey(),
+  contentId: d.integer().notNull().references(() => content.id, { onDelete: "cascade" }),
+  question: d.jsonb().notNull(),
+  createdAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+  updatedAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}));
+
+export const modelQuiz = pgTable("model_quiz_", (d) => ({
+  id: d.serial().primaryKey(),
+  contentId: d.integer().notNull().references(() => content.id, { onDelete: "cascade" }),
+  timeStamp: d.integer().notNull(), //in seconds
+  question: d.jsonb().notNull(),
+  createdAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+  updatedAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}));
+
+export const quizAttempts = pgTable("quiz_attempts", (d) => ({
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+  quizId: d.integer().notNull().references(() => endQuiz.id, { onDelete: "cascade" }),
+  score: d.integer().notNull(),
+  attemptedAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}));
+
+
+export const enrollments = pgTable("enrollments", (d) => ({
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+  courseId: d.integer().notNull().references(() => course.id, { onDelete: "cascade" }),
+  deadline: d.integer().default(0),//in days // 0 means no deadline 
+  enrolledAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}), (t) => ({
+  userCourseUnique: uniqueIndex('user_course_unique_idx').on(t.userId, t.courseId)
+}));
+
+export const progress = pgTable("progress", (d) => ({
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+  contentId: d.integer().notNull().references(() => content.id, { onDelete: "cascade" }),
+  completedAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}), (t) => ({
+  userContentUnique: uniqueIndex('user_content_unique_idx').on(t.userId, t.contentId)
+}));
+
+export const feedback = pgTable("feedback", (d) => ({
+  id: d.serial().primaryKey(),
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+  courseId: d.integer().notNull().references(() => course.id, { onDelete: "cascade" }),
+  rating: d.integer(),
+  comments: d.text(),
+  createdAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}));
+
+export const speedLogs = pgTable("speed_logs", (d) => ({
+  id: d.serial().primaryKey(),
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+  contentId: d.integer().notNull().references(() => content.id, { onDelete: "cascade" }),
+  event: videoEvents("event").notNull(),
+  speed: d.integer(),//round(speed * 100)
+  loggedAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}));
+
+
+// gamification tables 
+
+export const steak = pgTable("steak_", (d) => ({
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+  count: d.integer().notNull(),
+  date: d.date().notNull(),
+  createdAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+  updatedAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}), (t) => ({
+  userDateUnique: uniqueIndex('user_date_unique_idx').on(t.userId, t.date)
+}));
+
+export const xp = pgTable("xp_", (d) => ({
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+  xp: d.integer().notNull(),
+  createdAt: d.timestamp().$defaultFn(() => new Date()).notNull(),
+}));
+
+export const badge = pgTable("badge_", (d) => ({
+  id: d.serial().primaryKey(),
+  image: d.text(),
+  title: d.text().unique(),
+  description: d.text(),
+  conditions: d.jsonb()  
+}))
+
+export const badgeAssignment = pgTable("badge_assignment", (d) => ({
+  userId: d.text().notNull().references(() => user.id, { onDelete: "cascade" }),
+  badgeId: d.integer().notNull().references(() => badge.id, { onDelete: "cascade" }),
+  assignedAt: d.timestamp()
+}))
+
